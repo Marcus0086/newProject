ae090a7a701f332960265188940b8b8356905a3f
Marcus0086 <43565099+Marcus0086@users.noreply.github.com>
2020-09-24 20:34:54 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import Sorting from './components/sorting';
 //import Component2 from './components/component2';
-import Form from './src/components/form';
+import Form from './components/form';
 
 function App() {
     return (
