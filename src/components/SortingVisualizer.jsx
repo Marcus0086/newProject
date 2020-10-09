e28606fcfd4d22793c5641caf35953d547425f16
Marcus0086 <marcgupta456@gmail.com>
2020-10-10 01:04:56 +0530

    donenewmerge

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import "./SortingVisualizer.css";
 import MergeSort from '../Algorithms/mergesort';
+import { getMergeSortAnimations } from '../Algorithms/newmergesort';
 import binaySearch from '../Algorithms/binarysearch';
 //import { Form } from "react-bootstrap";
+
+const ANIMATION_SPEED_MS = 5;
+const PRIMARY_COLOR = '#14CADB';
+const SECONDARY_COLOR = 'red';
 class Sorting extends Component {
     constructor(props) {
         super(props);
@@ -29,18 +34,61 @@ class Sorting extends Component {
 
     resetArray() {
         const array = [];
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 300; i++) {
             array.push(randomIntFromFunction(50, 400));
         }
         this.setState({ array: array });
+
+        window.addEventListener('load', this.mergeSort());
     }
-    mergesortAlgo() {
+    /*mergesortAlgo() {
         var array = [];
-        const preSort = this.state.array.sort();
+        const preSort = this.state.array.slice().sort((a, b) => a - b);
         array = MergeSort(this.state.array);
         console.log("Merge Sort" + array);
         console.log("Pre Sort" + preSort);
         this.setState({ array });
+        (array.length === preSort.length) ? console.log(true) : console.log(false);
+    }*/
+
+    mergeSort() {
+        const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
+    }
+    testAlgos() {
+        for (let i = 0; i < 1000; i++) {
+            const arr = [],
+                length = randomIntFromFunction(1, 1000);
+            for (let j = 0; j < length; j++) {
+                arr.push(randomIntFromFunction(1, 1000));
+            }
+            const sortedArray = MergeSort(arr);
+            const presort = arr.slice().sort((a, b) => a - b);
+            //console.log(sortedArray);
+            //console.log(presort);
+            (sortedArray.length === presort.length) ? console.log(true) : console.log(false);
+            //console.log(sortedArray[5]);
+            //console.log(presort[5]);
+        }
     }
 
     binarysortAlgo() {
@@ -71,7 +119,8 @@ class Sorting extends Component {
                 ))}
                 </div>
                 <button onClick={() => this.resetArray()}>Reset</button>
-                <button onClick={() => this.mergesortAlgo()}>MergeSort</button>
+                <button onClick={() => this.mergeSort()}>MergeSort</button>
+                <button onClick={() => this.testAlgos()}>test</button>
             </React.Fragment>
         );
     }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,8 +4,14 @@ import HeroPage from '../components/hero';
 function HomePage(props) {
     return (
         <React.Fragment>
-            <HeroPage title={props.title} subTitle={props.subTitle} text={props.text}/>
-            <Sorting />
+            <HeroPage title={props.title} subTitle={props.subTitle} text={props.text} />
+            <div className="boxMain">
+                <h1>Visualization</h1>
+                <div className="box">
+                    <Sorting />
+                </div>
+                <div className="box" />
+            </div>
         </React.Fragment>
     );
 }
